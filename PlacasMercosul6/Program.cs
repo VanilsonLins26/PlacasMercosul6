50555698edc279e5dfa6dc65fed3e0afe12627a0
VanilsonLins26 <vanilson.liins@gmail.com>
2024-04-30 11:46:17 -0300

    final final

--- PlacasMercosul6/Program.cs
+++ PlacasMercosul6/Program.cs
@@ -4,20 +4,20 @@ using System.Globalization;
 using System.Linq;
 using System.Text.RegularExpressions;
 
-string pattern = @"^[A-Z]{3}\d{1}[A-Z]\d{2}$";
+string pattern = @"^[A-Z]{3}\d{1}[A-Z]\d{2}$"; // string no formato das placas do mercosul (3 letras, 1 numero, 1 letra , 2 letras)
 string result = "";
 int cond = 1;
 
-while (cond == 1)
+while (cond == 1) // repetição para realizar quantas verificações o usuario quiser.
 {
     Console.WriteLine("Insira a placa");
     string x = Console.ReadLine();
-    x = x.ToUpper();
-    string y = x.Substring(0, 3);
+    x = x.ToUpper();   //transforma tudo em letra maiuscula para fazer a verificação
+    string y = x.Substring(0, 3); //separa apenas a 3 primeiras letras, o necessário para realizar a verificação
 
-    if (Regex.IsMatch(x, pattern, RegexOptions.IgnoreCase))
+    if (Regex.IsMatch(x, pattern, RegexOptions.IgnoreCase)) // compara a placa inserida com a string padrao( para verificar se está no formato certo)
     {
-        if ((Regex.IsMatch(y, @"^[N][E][I-Z]$"))
+        if ((Regex.IsMatch(y, @"^[N][E][I-Z]$"))  //compara com todas as possibilidades de placas em cada estado
             || (Regex.IsMatch(y, @"^[N][F][A-B]$"))
             || (Regex.IsMatch(y, @"^[Q][L][N-T]$"))
             || (Regex.IsMatch(y, @"^[S][A][K-M]$")))
@@ -52,12 +52,12 @@ while (cond == 1)
     else
         result = "Placa inválida";
 
-    Console.WriteLine(result);
+    Console.WriteLine(result); // mostra o resultado da consulta
     Console.WriteLine();
 
     do
     {
-        Console.WriteLine("1) Fazer nova consulta \n2) Sair ");
+        Console.WriteLine("1) Fazer nova consulta \n2) Sair "); //pergunta ao usuário se quer realizar outra consulta ou encerrar o programa
         cond = int.Parse(Console.ReadLine());
         Console.WriteLine();
 
--- PlacasMercosul6/obj/Release/net8.0/PlacasMercosul6.AssemblyInfo.cs
+++ PlacasMercosul6/obj/Release/net8.0/PlacasMercosul6.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlacasMercosul6")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f51b025cdbb0f1cb33be8a8cf46a7ba0a3d25d6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlacasMercosul6")]
